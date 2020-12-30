d9850de93cb06646cea61081c63a2e1df29aa66f
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2020-12-31 01:05:05 +0200

    Add files via upload

---
+++ www/mysql-connect.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>hussein zayed</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<body>
+<br>
+<br>
+<br>
+<center> <h1>Hussein ramadan zayed </h1>   </center>
+<center> <a href="show-table.php"> check conect with database </a>   
+
+
+
+</body>
+</html>
+
+
